5e7b76445de8b92b65518ec4bbbf65fa6acf165a
mradi <2batou4u@gmail.com>
2023-06-06 20:43:47 +0200

    Fixed indent in deleteAllProjectTables() query

--- projektübung/dbs2023ExerciseProject.py
+++ projektübung/dbs2023ExerciseProject.py
@@ -81,13 +81,14 @@ class DatabaseProject:
 
             cursor.execute(f"""
             SET FOREIGN_KEY_CHECKS=0;
-		    SET @tables = NULL;
-            SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name,'`') INTO @tables FROM information_schema.tables WHERE table_schema = 'fantasy_welt';
-		    SET @tables = CONCAT('DROP TABLE ', @tables);
-		    PREPARE stmt1 FROM @tables;
-		    EXECUTE stmt1;
-		    DEALLOCATE PREPARE stmt1;
-		    SET FOREIGN_KEY_CHECKS=1;
+            SET @tables = NULL;
+            SELECT GROUP_CONCAT('`', table_schema, '`.`', table_name,'`') INTO @tables FROM information_schema.tables 
+            WHERE table_schema = 'fantasy_welt';
+            SET @tables = CONCAT('DROP TABLE ', @tables);
+            PREPARE stmt1 FROM @tables;
+            EXECUTE stmt1;
+            DEALLOCATE PREPARE stmt1;
+            SET FOREIGN_KEY_CHECKS=1;
             """)
 
         except mariadb.IntegrityError as i_err:
